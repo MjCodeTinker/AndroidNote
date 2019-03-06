e5337a0a2d683b3fc8457daa9e9fe5fa6be1e8fc
miaojian <miaojian@cmcm.com>
2019-03-06 22:36:16 +0800

    【Add】 完善消息传递框架
    【描述】:
    【影响范围(模块)】:无
    【测试建议】:无
    【自测通过】:通过
    【风险(可选)】:无
    【reviewed by】:self

--- LibBase/src/main/java/com/mj/lib/base/communication/app_inner/MessageTrain.java
+++ LibBase/src/main/java/com/mj/lib/base/communication/app_inner/MessageTrain.java
@@ -1,14 +1,16 @@
 package com.mj.lib.base.communication.app_inner;
 
+import android.os.Handler;
+import android.os.Looper;
 import com.mj.lib.base.communication.app_inner.annotation.Subscriber;
 import com.mj.lib.base.communication.app_inner.entity.MethodInfo;
 import com.mj.lib.base.communication.app_inner.type.ThreadMode;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Author      : MJ
@@ -24,8 +26,16 @@ public class MessageTrain {
     // 所有回调方法集合与对象的映射关系
     private Map<Object, List<MethodInfo>> registers;
 
+    // 主线程handler
+    private Handler mainHandler;
+
+    // 线程池
+    private ExecutorService executorService;
+
     private MessageTrain() {
         registers = new HashMap<>();
+        mainHandler = new Handler(Looper.getMainLooper());
+        executorService = Executors.newSingleThreadExecutor();
     }
 
     public static MessageTrain getDefault() {
@@ -40,10 +50,57 @@ public class MessageTrain {
     }
 
 
-    public void post(Object message) {
-        List<MethodInfo> methodInfoList = registers.get(message);
-        if (methodInfoList != null) {
-            registers.remove(message);
+    /**
+     * 发送消息
+     *
+     * @param message 要发送的消息
+     */
+    public void post(final Object message) {
+
+        Set<Object> keySet = registers.keySet();
+        Iterator<Object> iterator = keySet.iterator();
+
+        if (iterator.hasNext()) {
+            final Object subscriberClass = iterator.next();
+            List<MethodInfo> methodInfoList = registers.get(message);
+            for (MethodInfo methodInfo : methodInfoList) {
+
+                final Method method = methodInfo.getMethod();
+                final Class<?> params = methodInfo.getParams();
+                ThreadMode threadMode = methodInfo.getThreadMode();
+                // 判断参数 是否一样（包含父类 父接口）
+                if (params.isAssignableFrom(message.getClass())) {
+
+                    if (ThreadMode.MAIN == threadMode) {
+
+                        if (Looper.myLooper() == Looper.getMainLooper()) {
+                            postMessage(method, subscriberClass, message);
+                        } else {
+                            mainHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    postMessage(method, subscriberClass, message);
+                                }
+                            });
+                        }
+
+                    } else if (ThreadMode.WORK_THREAD == threadMode) {
+
+                        if (Looper.myLooper() == Looper.getMainLooper()) {
+                            executorService.execute(new Runnable() {
+                                @Override
+                                public void run() {
+                                    postMessage(method, subscriberClass, message);
+                                }
+                            });
+                        } else {
+                            postMessage(method, subscriberClass, message);
+                        }
+
+                    }
+                }
+
+            }
         }
     }
 
@@ -61,10 +118,34 @@ public class MessageTrain {
         }
     }
 
+    /**
+     * 接触注册
+     *
+     * @param obj 那个类
+     */
     public void unRegister(Object obj) {
-
+        List<MethodInfo> methodInfoList = registers.get(obj);
+        if (methodInfoList != null) {
+            registers.remove(obj);
+        }
     }
 
+
+    /**
+     * 调用注册注册改消息的方法
+     *
+     * @param method  method
+     * @param message 参数
+     */
+    private void postMessage(Method method, Object subscriberClass, Object message) {
+        try {
+            method.invoke(subscriberClass, message);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * 查找某个类中包含 subscriber注解的方法
@@ -89,6 +170,8 @@ public class MessageTrain {
                 Class<?> parameterType = parameterTypes[0];
                 MethodInfo methodInfo = new MethodInfo(parameterType, threadMode, method);
                 methodInfoList.add(methodInfo);
+            } else {
+                throw new RuntimeException("only allow has parameter");
             }
         }
         return methodInfoList;
