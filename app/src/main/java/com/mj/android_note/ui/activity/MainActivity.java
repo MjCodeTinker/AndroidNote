5703dc43506e554c74338975fafecc57b9ed9389
miaojian <miaojian@cmcm.com>
2019-04-19 12:18:25 +0800

    【Add】android 属性动画代码演示
    【描述】:
    【影响范围(模块)】:无
    【测试建议】:无
    【自测通过】:通过
    【风险(可选)】:无
    【reviewed by】:self

--- app/src/main/java/com/mj/android_note/ui/activity/MainActivity.java
+++ app/src/main/java/com/mj/android_note/ui/activity/MainActivity.java
@@ -1,18 +1,19 @@
 package com.mj.android_note.ui.activity;
 
+import android.animation.*;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.View;
-import com.airbnb.lottie.LottieAnimationView;
+import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.TextView;
 import com.mj.android_note.R;
-import com.mj.android_note.dispatch_touch.DispatchTouchActivity;
-import com.mj.android_note.libary.adaptive.StartSystemSettingPageImpl;
-import com.mj.android_note.libary.adaptive.SystemSettingPageEntity;
-import com.mj.android_note.module.process.MainProcessActivity;
-import com.mj.android_note.ui.activity.db.DbMainActivity;
 import com.mj.lib.base.communication.app_inner.MessageTrain;
+import com.mj.lib.base.log.LogUtil;
 
 /**
  * Author      : MJ
@@ -24,6 +25,9 @@ public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
 
+    ValueAnimator valueAnimator;
+
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,20 +35,20 @@ public class MainActivity extends Activity {
 
         MessageTrain.getDefault().register(this);
 
-        final LottieAnimationView lottieView = findViewById(R.id.lottieView);
+//        final LottieAnimationView lottieView = findViewById(R.id.lottieView);
 //        lottieView.setImageAssetsFolder("images/");
 //        lottieView.setImageAssetsFolder("/");
 //        lottieView.setImageAssetsFolder();
 //        lottieView.setAnimation("data.json");
 //        lottieView.loop(true);
 
-        findViewById(R.id.main_activity_btn_db).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DbMainActivity.class);
-                startActivity(intent);
-            }
-        });
+//        findViewById(R.id.main_activity_btn_db).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, DbMainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 //        lottieView.setImageAssetsFolder("lottie/images/");
 //        Cancellable cancellable = LottieComposition.Factory.fromAssetFileName(this, "lottie/data.json", new OnCompositionLoadedListener() {
 //            @Override
@@ -63,29 +67,111 @@ public class MainActivity extends Activity {
 //            }
 //        });
 
-        findViewById(R.id.main_activity_btn_permission).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SystemSettingPageEntity adaptationEntity = new SystemSettingPageEntity();
-                adaptationEntity.setContext(MainActivity.this);
-                new StartSystemSettingPageImpl().beganToFit(adaptationEntity);
-            }
-        });
+//        findViewById(R.id.main_activity_btn_permission).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                SystemSettingPageEntity adaptationEntity = new SystemSettingPageEntity();
+//                adaptationEntity.setContext(MainActivity.this);
+//                new StartSystemSettingPageImpl().beganToFit(adaptationEntity);
+//            }
+//        });
 
-        findViewById(R.id.main_activity_btn_process_communication).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MainProcessActivity.launcher(MainActivity.this);
-            }
-        });
+//        findViewById(R.id.main_activity_btn_process_communication).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                MainProcessActivity.launcher(MainActivity.this);
+//            }
+//        });
+//
+//        final Button btnView = findViewById(R.id.main_activity_btn_dispatch_touch);
+//        btnView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                valueAnimator.start();
+////                DispatchTouchActivity.launcher(MainActivity.this);
+//            }
+//        });
 
-        findViewById(R.id.main_activity_btn_dispatch_touch).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DispatchTouchActivity.launcher(MainActivity.this);
-            }
-        });
-
+        testValueAnimator();
+        testObjectAnimator();
     }
+
+    private void testObjectAnimator() {
+        int [] colorChangeArray = {
+                getResources().getColor(R.color.colorPrimary),
+                getResources().getColor(R.color.white),
+                getResources().getColor(R.color.colorAccent)};
+        final ObjectAnimator objectAnimator = ObjectAnimator.ofInt(getWindow(),"statusBarColor", colorChangeArray);
+        View btnObjAnim = findViewById(R.id.main_activity_btn_obj_anim_test);
+        btnObjAnim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                objectAnimator.start();
+            }
+        });
+        objectAnimator.setEvaluator(new ArgbEvaluator());
+        objectAnimator.setDuration(5000);
+    }
+
+    private void testValueAnimator() {
+        final TextView btnAnimView = findViewById(R.id.main_activity_btn_anim_test);
+
+        btnAnimView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                valueAnimator.start();
+            }
+        });
+
+        final XYTranslation xyTranslationStart = new XYTranslation();
+        btnAnimView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                xyTranslationStart.x = btnAnimView.getWidth();
+                xyTranslationStart.y = btnAnimView.getHeight();
+                btnAnimView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+            }
+        });
+        XYTranslation xyTranslationEnd = new XYTranslation();
+        xyTranslationEnd.x = 700;
+        xyTranslationEnd.y = 350;
+
+        TypeEvaluator<XYTranslation> xyTranslationTypeEvaluator = new TypeEvaluator<XYTranslation>() {
+            @Override
+            public XYTranslation evaluate(float fraction, XYTranslation startValue, XYTranslation endValue) {
+                LogUtil.d(TAG, "fraction = " + fraction);
+                XYTranslation xyTranslation = new XYTranslation();
+                xyTranslation.x = (int) (startValue.x + (endValue.x - startValue.x) * fraction);
+                xyTranslation.y = (int) (startValue.y + (endValue.y - startValue.y) * fraction);
+                return xyTranslation;
+            }
+        };
+
+        valueAnimator = ValueAnimator.ofObject(xyTranslationTypeEvaluator, xyTranslationStart, xyTranslationEnd);
+        valueAnimator.setInterpolator(new TimeInterpolator() {
+            @Override
+            public float getInterpolation(float input) {
+                LogUtil.d(TAG, "input = " + input);
+                return input;
+            }
+        });
+        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                XYTranslation animatedValue = (XYTranslation) animation.getAnimatedValue();
+                LogUtil.d(TAG, "animatedValue x = " + animatedValue.x + "-- animatedValueY = " + animatedValue.y);
+                btnAnimView.getLayoutParams().width = animatedValue.x;
+                btnAnimView.getLayoutParams().height = animatedValue.y;
+                btnAnimView.requestLayout();
+            }
+        });
+        valueAnimator.setDuration(1000);
+    }
+
+    class XYTranslation {
+        int x;
+        int y;
+    }
+
 
 }
