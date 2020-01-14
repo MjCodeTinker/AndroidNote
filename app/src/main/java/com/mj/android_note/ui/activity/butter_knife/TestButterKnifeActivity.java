09a4105ad43b7004500e82dc18ab8f32b5d40ead
mj <jianmiao@galaxyinternet.com>
2020-01-14 22:12:30 +0800

    生成butter_knife注册点击事件的代码

--- app/src/main/java/com/mj/android_note/ui/activity/butter_knife/TestButterKnifeActivity.java
+++ app/src/main/java/com/mj/android_note/ui/activity/butter_knife/TestButterKnifeActivity.java
@@ -1,15 +1,18 @@
 package com.mj.android_note.ui.activity.butter_knife;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mj.android_note.R;
+import com.mj.android_note.utils.ToastUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 
@@ -24,6 +27,12 @@ public class TestButterKnifeActivity extends AppCompatActivity {
     @BindView(R.id.activity_test_butter_knife_tv)
     TextView tv;
 
+    @OnClick(R.id.activity_test_butter_knife_tv)
+    void textViewClick(View v) {
+        ToastUtils.showShortToast("我是textView 我被点击了：" + tv.getText());
+    }
+
+
     // bind
     private Unbinder bind;
 
@@ -33,7 +42,7 @@ public class TestButterKnifeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test_butter_knife);
         bind = ButterKnife.bind(this);
 
-        tv.setText(String.format("%s","i am miaojian"));
+        tv.setText(String.format("%s", "i am miaojian"));
     }
 
     @Override
