2583f3a3492f2878ba05d91dff65b392cbbef4f7
miaojian <miaojian@cmcm.com>
2020-01-15 11:59:10 +0800

    【Add】检测一个数列是否为单调数列
    【描述】:
    【影响范围(模块)】:无
    【测试建议】:无
    【自测通过】:通过
    【风险(可选)】:无
    【reviewed by】:self

--- app/src/main/java/com/mj/android_note/data_structure/algorithm/base/BaseTestDemo.java
+++ app/src/main/java/com/mj/android_note/data_structure/algorithm/base/BaseTestDemo.java
@@ -1,7 +1,5 @@
 package com.mj.android_note.data_structure.algorithm.base;
 
-import java.util.Random;
-
 public class BaseTestDemo {
 
 
@@ -11,16 +9,21 @@ public class BaseTestDemo {
         BaseAlgorithm baseAlgorithm = new BaseAlgorithm();
 
         // 冒泡排序
-        int[] count = new int[10000];
-        Random random = new Random();
-        baseAlgorithm.printData(count);
-        System.out.println("bubbling sort ###########");
+//        int[] count = new int[10000];
+//        Random random = new Random();
+//        baseAlgorithm.printData(count);
+//        System.out.println("bubbling sort ###########");
+//
+//        for (int i = 0; i < count.length; i++) {
+//            count[i] = random.nextInt(10000);
+//        }
+//
+//        baseAlgorithm.printData(baseAlgorithm.bubbling(count));
 
-        for (int i = 0; i < count.length; i++) {
-            count[i] = random.nextInt(10000);
-        }
-
-        baseAlgorithm.printData(baseAlgorithm.bubbling(count));
+        // 判断一个数列是否为单调数列
+        boolean b = baseAlgorithm.checkArrayIsMonotonous(new int[]{1, 3, 1});
+        boolean b1 = baseAlgorithm.checkArrayIsMonotonous(new int[]{1, 2, 3});
+        System.out.println("b = " + b + "--b1 = " + b1);
 
     }
 
