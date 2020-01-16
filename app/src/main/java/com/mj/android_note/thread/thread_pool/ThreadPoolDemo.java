7d83d40c75ef7ade803aaa4a2ad0a790659d83f7
miaojian <miaojian@cmcm.com>
2020-01-16 19:31:40 +0800

    【Add】线程池管理
    【描述】:
    【影响范围(模块)】:无
    【测试建议】:无
    【自测通过】:通过
    【风险(可选)】:无
    【reviewed by】:self

--- app/src/main/java/com/mj/android_note/thread/thread_pool/ThreadPoolDemo.java
+++ app/src/main/java/com/mj/android_note/thread/thread_pool/ThreadPoolDemo.java
@@ -6,6 +6,8 @@ import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ThreadPoolDemo {
 
@@ -43,6 +45,7 @@ public class ThreadPoolDemo {
         // 创建一个单线程的线程池，遵循FIFO,先进先出原则，如果任务数大于线程数，则在队列中等待，适用于数据库操作，文件操作，不能多线程访问，且阻塞UI线程的操作
         ExecutorService executorService2 = Executors.newSingleThreadExecutor();
 
+        executorService.shutdown();
 
         // 线程池的工作原理:
         // 依据参数，就可以看出线程池的工作原理
@@ -64,9 +67,119 @@ public class ThreadPoolDemo {
                 TimeUnit.SECONDS,
                 new LinkedBlockingQueue<>());
 
+        //########## 线程池中的锁 #########
+
+        // 多线程做i++操作
+        MyEntity myEntity = new MyEntity();
+        for (int i = 0; i < 10; i++) {
+            new Thread(myEntity::addForSynchronized).start();
+        }
+        try {
+            Thread.sleep(2000);
+            printLog("num : " + myEntity.getNum());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 为什么多线程处理i++ 操作，不加锁保护，会出现最终只不符合我们的预期
+        // 因为：
+        // 1. java内存模型中，各个线程会将共享变量从主存中拷贝到工作内存中
+        // 2. 做i++操作的时候是基于工作内存中的变量来做的修改，比如当前线程中 i 为999 当时另一个线程已经将i 修改为1000，但是当前线程还是按照999做i++ 的操作
+        // 工作内存修改之后，合适写入到主内存中？
+        // 1. 这个时机对于普通变量是没有规定的，而针对volatile 修饰的变量给java虚拟机特殊的约定
+        // 2. 线程对volatile变量修改之后会立刻被其他线程锁感知，即不会出现脏数据的现象，从而保证数据的“可见性”
+
+        //################ 关键字 volatile ###################
+        // 工作内存修改之后，何时写入到主内存中？
+        // 1. 这个时机对于普通变量是没有规定的，而针对volatile 修饰的变量给java虚拟机特殊的约定
+        // 2. 线程对volatile变量修改之后会立刻被其他线程锁感知，即不会出现脏数据的现象，从而保证数据的“可见性”
+
+        // volatile 是如何保证数据可见性的？
+        // 1.生成汇编的时候，会对volatile修饰的共享变量进行写操作的时候会多出Lock前缀的指令
+        // 2.Lock前缀指令会引起处理器缓存写回到主存
+        // 3.处理器的缓存写回到主存中，会导致其他处理器中的缓存失效
+        // 4.其他处理器发现缓存失效，会在去主存中读取数据，即可以获取当前的最新值，这样就能保证每个线程都能获得该变量的最新值
+
+        // 线程有几种状态，分别是什么条件下触发
+        //
+
+        //sleep 、wait 、join、yield,interrupt
+
+        // wait
+        //interrupt
+        //interrupt 可能会导致死锁，比如一个线程中正在用lock,此时如果做中断操作，锁将无法被释放
+        //reentrantLock 提供了 tryLock的方法
+
+
+        // Semaphore
+
+        Thread t = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+            }
+        };
+        t.interrupt();
+        Object b = new Object();
+        try {
+            b.wait();
+            b.notify();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
     }
 
+    // 多线程做 i++ 操作 实现的三种方式
+    private static class MyEntity {
+        private volatile int num = 0;
+
+        private AtomicInteger atomicNum = new AtomicInteger(0);
+
+        private volatile ReentrantLock reentrantLock;
+
+        public MyEntity() {
+            this.reentrantLock = new ReentrantLock(false);
+        }
+
+        // 锁住方法，一个线程一个线程调用
+        synchronized void addForSynchronized() {
+            for (int i = 0; i < 10000; i++) {
+                num++;
+            }
+            printLog("ThreadName : " + Thread.currentThread().getName() + "---num=" + num);
+        }
+
+        // 通过可重入锁，来做线多线程同步的操作
+        void addForReentrantLock() {
+
+            for (int i = 0; i < 10000; i++) {
+                reentrantLock.lock();
+                try {
+                    num++;
+                }finally {
+                    reentrantLock.unlock();
+                }
+            }
+            printLog("ThreadName : " + Thread.currentThread().getName() + "---num=" + num);
+        }
+
+
+        // 通过原子性AtomicInteger来完成
+        // 原理是：1.是通过unsafe 来实现的同步问题，unsafe是java中获取对象内存地址的类，他的作用就是比较并且替换 （CAS ）
+        // 就是在读取的时候，循环等待的操作，检测读取后和写入时候，值是否发生了改变，更新操作在内存中找到value的位置，方便比较
+        // volatile 修饰了AtomicInteger中的value，就是保证了在更新操作时，当前线程可以拿到value最新的值。
+        void AddForAtomicInteger() {
+            for (int i = 0; i < 10000; i++) {
+                atomicNum.getAndIncrement();
+            }
+            printLog("ThreadName : " + Thread.currentThread().getName() + "---atomicNum=" + atomicNum.get());
+        }
+
+        private int getNum() {
+            return num;
+        }
+    }
 
     private static void printLog(String msg) {
         System.out.println("###" + msg);
