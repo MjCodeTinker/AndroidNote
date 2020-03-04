d861b2958a99feb63261ae67aa0c45046d8503f1
miaojian <miaojian@cmcm.com>
2020-03-04 14:46:26 +0800

    插件化demo

---
+++ app/src/main/java/com/mj/android_note/base/plugins_test/MockPluginActivity.java
@@ -0,0 +1,4 @@
+package com.mj.android_note.base.plugins_test;
+
+public class MockPluginActivity {
+}
---
+++ app/src/main/java/com/mj/android_note/base/plugins_test/PluginsHelper.java
@@ -0,0 +1,8 @@
+package com.mj.android_note.base.plugins_test;
+
+/**
+ * 负责加载插件
+ */
+public class PluginsHelper {
+
+}
