b44cda5e17cc9bc381db8dc909ebbdceda9e6a81
miaojian <jianmiao@galaxyinternet.com>
2020-04-03 01:03:06 +0800

    java判断一个字符是否为数字，或字母

--- app/src/main/java/com/mj/android_note/data_structure/algorithm/base/str/TestStringAlgorithm.java
+++ app/src/main/java/com/mj/android_note/data_structure/algorithm/base/str/TestStringAlgorithm.java
@@ -13,6 +13,20 @@ public class TestStringAlgorithm {
         //##1，把这个字符串拷贝到androidStudio中  遇到一个大坑，网页中的空格与编译器里的编码格式不一样？？？，这个空格要重新写一下
         String reverseWords = reverseWords("  hello world!  ");
         print("reverseWords :" + "##" + reverseWords + "##");
+        print("" + Character.isDigit('1') + "--" + Character.isDigit('a'));
+        char judgeChar1 = '1';
+        char judgeCHar2 = 'x';
+        print("target 1 : " + isLetter(judgeChar1) + "--target 2 : " + isLetter(judgeCHar2));
+    }
+
+    /**
+     * 判断一个字符是否为数字
+     *
+     * @param target 目标字符
+     * @return 是否为字母
+     */
+    private static boolean isLetter(char target) {
+        return Character.isLowerCase(target) || Character.isUpperCase(target);
     }
 
 
@@ -36,7 +50,6 @@ public class TestStringAlgorithm {
                 }
             }
         }
-        result.toString().substring()
         return result.toString();
     }
 
