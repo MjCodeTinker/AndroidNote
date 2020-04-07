5b2398bb589ac68e0763b7a4e3597b5658e5df1a
miaojian <jianmiao@galaxyinternet.com>
2020-04-07 10:46:08 +0800

    java字符串中找出最大数

--- app/src/main/java/com/mj/android_note/data_structure/algorithm/base/str/TestStringAlgorithm.java
+++ app/src/main/java/com/mj/android_note/data_structure/algorithm/base/str/TestStringAlgorithm.java
@@ -1,5 +1,10 @@
 package com.mj.android_note.data_structure.algorithm.base.str;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Author      : MJ
  * Date        : 2020-04-02--23:09
@@ -17,6 +22,12 @@ public class TestStringAlgorithm {
         char judgeChar1 = '1';
         char judgeCHar2 = 'x';
         print("target 1 : " + isLetter(judgeChar1) + "--target 2 : " + isLetter(judgeCHar2));
+//        print("字符串转Int : " +  strToInt("160"));
+        print("查找字符串中最大数 ： " + findStrMaxNumber("a - #" + 10 + "a16&18"));
+        List<Integer> a = new ArrayList<>();
+        String[] objects = (String[]) a.toArray();
+        Collections.reverse(a);
+
     }
 
     /**
@@ -57,6 +68,84 @@ public class TestStringAlgorithm {
     private static void print(String str) {
         System.out.println(str);
         System.out.println("#########---分隔---#########");
+    }
+
+
+    private static int strToInt(String str) {
+        str = str.trim();
+        if (str.length() == 0) {
+            return 0;
+        }
+        // 如果第一个字符不是一个数字，并且也不是 + -
+        if (!Character.isDigit(str.charAt(0)) && !isPlusOrMinus(str.charAt(0))) {
+            return 0;
+        }
+
+        StringBuilder result = new StringBuilder();
+        result.append(str.charAt(0));
+        String findResult = String.valueOf(findStrMaxNumber(str));
+        result.append(findResult);
+
+        if (isMinus(str.charAt(0)) && String.valueOf(Integer.MIN_VALUE).length() < result.length()) {
+            return Integer.MIN_VALUE;
+        }
+        if (!isMinus(str.charAt(0)) && String.valueOf(Integer.MAX_VALUE).length() < result.length()) {
+            return Integer.MAX_VALUE;
+        }
+        try {
+            return Integer.parseInt(result.toString());
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    // 判断是否为正负号
+    private static boolean isPlusOrMinus(char target) {
+        return isPlus(target) || isMinus(target);
+    }
+
+    private static boolean isPlus(char target) {
+        return target == '+' || target > '0';
+    }
+
+    private static boolean isMinus(char target) {
+        return target == '-';
+    }
+
+    // 找出字符串中最长数字的字符串
+    private static int findStrMaxNumber(String str) {
+        str = str.trim();
+        int result = 0;
+        if (str.equals("")) {
+            return result;
+        }
+        // 以非数字进行分割
+        String[] splitArray = str.split("\\D");
+        boolean isFirstMinus = true;
+
+        for (String s : splitArray) {
+            String tempStr = s.trim();
+            if (!tempStr.equals("")) {
+                int tempInt = 0;
+                try {
+                    tempInt = Integer.parseInt(tempStr);
+                } catch (Exception e) {
+                    // 不做处理，
+                }
+                if (tempInt < 0) {
+                    // 第一次拿到负值
+                    if (isFirstMinus) {
+                        result = tempInt;
+                        isFirstMinus = false;
+                    } else if (tempInt > result) {
+                        result = tempInt;
+                    }
+                } else if (tempInt > result) {
+                    result = tempInt;
+                }
+            }
+        }
+        return result;
     }
 
 }
